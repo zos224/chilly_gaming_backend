cc36574bbc7e5e0105b5720f31039e62c4b5c609
zos224 <95456855+zos224@users.noreply.github.com>
2022-12-04 05:23:39 +0700

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -26,14 +26,6 @@
 //                 'avatar_url' => 'default.jpg',
 //                 'role' => 0
 //             ]);
-            $user = new User([
-                'username' => 'phong224',
-                'email' => 'ngophong@gmail.com',
-                'password' => bcrypt('Phong@224'),
-                'avatar_url' => 'default.jpg',
-                'role' => 0
-            ]);
-            $user->save();
             if ($user)
             {
                 return response(['alo' => 'co user ne ma', 'user' => $user]);
