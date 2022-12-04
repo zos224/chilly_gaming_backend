982d5ae3be389c8215ffbceabf2c9b9d82776a89
zos224 <95456855+zos224@users.noreply.github.com>
2022-12-04 15:33:58 +0700

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -19,20 +19,29 @@
                     Password::min(8)->mixedCase()->numbers()->symbols()
                 ]
             ]);
-            $user = User::create([
-                'username' => $data['username'],
+            
+//             $user = User::create([
+//                 'username' => $data['username'],
+//                 'email' => $data['email'],
+//                 'password' => bcrypt($data['password']),
+//                 'avatar_url' => 'default.jpg',
+//                 'role' => 0
+//             ]);
+            
+            $user = new User([
+                    'username' => $data['username'],
                 'email' => $data['email'],
                 'password' => bcrypt($data['password']),
                 'avatar_url' => 'default.jpg',
                 'role' => 0
             ]);
-      
-            //$token = $user->createToken('main')->plainTextToken;
+        $user->save();
+            $token = $user->createToken('main')->plainTextToken;
             
-//             return response([
-//                 'user' => $user,
-//                 //'token' => $token
-//             ]);
+            return response([
+                'user' => $user,
+                'token' => $token
+            ]);
         }
 
         public function login(Request $request)
